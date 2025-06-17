86fccfb454382b5f1b68200830219dce45116c3c
DannyIsWhatToDo <titouan.Vaillant@free.fr>
2025-06-17 16:13:21 +0200

    new test scene

---
+++ Assets/Script/sc_Camera_Move_TV.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class sc_Camera_Move_TV : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
